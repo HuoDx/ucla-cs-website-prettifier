d6d3b2e36a27dbc4dedf96417cf0b024484e1d7e
Daixuan <huodx@ucla.edu>
2021-10-28 15:22:48 -0700

    fix: bypassing the access restriction

--- prettier.js
+++ prettier.js
@@ -1,5 +1,23 @@
 async function pullContentFromSrc(url) {
-    return await (await fetch(url, {mode:'no-cors', referrerPolicy: 'same-origin', credentials: 'same-origin'})).text()
+    return await (await fetch(url, {
+        headers: {
+            'Host': 'web.cs.ucla.edu',
+            'Connection': 'keep-alive',
+            'Cache-Control': 'max-age=0',
+            'sec-ch-ua': `"Google Chrome";v="95", "Chromium";v="95", ";Not A Brand";v="99"`,
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': "macOS",
+            'Upgrade-Insecure-Requests': 1,
+            'User-Agent': `Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36`,
+            'Accept': `text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9`,
+            'Sec-Fetch-Site': 'none',
+            'Sec-Fetch-Mode': 'navigate',
+            'Sec-Fetch-User': '?1',
+            'Sec-Fetch-Dest': 'document',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Accept-Language': 'en-US,en;q=0.9'
+        }
+    })).text()
 }
 function setURLContentToMainContainer(url) {
     pullContentFromSrc(url).then(
