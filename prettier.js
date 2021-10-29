27711b29dd8bb3a75e741eba1b323a44099ffe0c
Daixuan <huodx@ucla.edu>
2021-10-28 19:35:26 -0700

    fix: url parsing

--- prettier.js
+++ prettier.js
@@ -1,29 +1,12 @@
 async function pullContentFromSrc(url) {
-    return await (await fetch(url, {
-        headers: {
-            'Host': 'web.cs.ucla.edu',
-            'Connection': 'keep-alive',
-            'Cache-Control': 'max-age=0',
-            'sec-ch-ua': `"Google Chrome";v="95", "Chromium";v="95", ";Not A Brand";v="99"`,
-            'sec-ch-ua-mobile': '?0',
-            'sec-ch-ua-platform': "macOS",
-            'Upgrade-Insecure-Requests': 1,
-            'User-Agent': `Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36`,
-            'Accept': `text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9`,
-            'Sec-Fetch-Site': 'none',
-            'Sec-Fetch-Mode': 'navigate',
-            'Sec-Fetch-User': '?1',
-            'Sec-Fetch-Dest': 'document',
-            'Accept-Encoding': 'gzip, deflate, br',
-            'Accept-Language': 'en-US,en;q=0.9'
-        }
-    })).text()
+    return await (await fetch(url)).text()
 }
 function setURLContentToMainContainer(url) {
     pullContentFromSrc(url).then(
         function (content) {
             document.getElementById('main-panel').innerHTML = content;
             document.getElementById('main-panel').querySelectorAll('a').forEach((element) => {
+                console.log(`${url}-> ${element.href}`);
                 element.target = '';
                 element.setAttribute('onclick', `setURLContentToMainContainer('${element.href}')`);
                 element.href = '#';
